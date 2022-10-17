1a6ec5b5c32ee729746bac802b10d7d54feb658f
dlscks <dlscks971026@gmail.com>
2022-10-17 17:35:17 +0900

    20221017 | up

---
+++ src/java010_abstract_interface/part07/Expand.java
@@ -0,0 +1,6 @@
+package java010_abstract_interface.part07;
+
+//Cannot override the final method from FinalTest
+public class Expand /*extends Base*/{
+
+}
---
+++ src/java010_abstract_interface/prob/part01/MyColorPoint.java
@@ -0,0 +1,16 @@
+package java010_abstract_interface.prob.part01;
+
+//public class MyColorPoint extends MyPoint{
+//int color;
+//	public MyColorPoint() {
+//
+//	}
+//	
+//	public MyColorPoint(String name, int x, int y) {
+//		super(name);
+//		
+//	}
+	
+	
+	
+//}
